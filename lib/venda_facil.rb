e1547447d78d5940f6aed6786da61601f291d990
Ernane Sena <ernane.sena@gmail.com>
2017-10-12 23:46:38 -0300

    Exemplos de como agir com namespaces

--- lib/venda_facil.rb
+++ lib/venda_facil.rb
@@ -1,30 +1,4 @@
 module VendaFacil
-  class Set
-    def initialize
-      @banco_de_arquivos = BancoDeArquivos.new
-    end
-
-    def adiciona(livro)
-      salva livro do
-        livros << livro
-      end
-    end
-
-    def livros_por_categoria(categoria)
-      livros.select { |livro| livro.categoria == categoria }.each do |livro|
-        yield livro if block_given?
-      end
-    end
-
-    def livros
-      @livros ||= @banco_de_arquivos.carrega
-    end
-
-    private
-
-    def salva(livro)
-      @banco_de_arquivos.salva livro
-      yield
-    end
+  module Array
   end
 end
