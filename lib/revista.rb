d78274f32766c120fef9f652c4655b2a91f146fd
Ernane Sena <ernane.sena@gmail.com>
2017-11-04 15:36:58 -0300

    Refatoração de camada de dados na classe Revista

--- lib/revista.rb
+++ lib/revista.rb
@@ -1,7 +1,30 @@
 class Revista
-  attr_reader :titulo
+  attr_reader :titulo, :id
+  attr_accessor :valor
 
-  def initialize(titulo)
+  def initialize(titulo, valor)
     @titulo = titulo
+    @valor = valor
+    @id = next_id
+  end
+
+  def save
+    File.open("db/revistas/#{@id}.yml", 'w') do |file|
+      file.puts serialize
+    end
+  end
+
+  def self.find(id)
+    YAML.load File.open("db/revistas/#{id}.yml", 'r')
+  end
+
+  private
+
+  def serialize
+    YAML.dump self
+  end
+
+  def next_id
+    Dir.glob('db/revistas/*.yml').size + 1
   end
 end
--- sample.rb
+++ sample.rb
@@ -1,18 +1,63 @@
 require File.expand_path('lib/loja_virtual')
 
 livros = [
-  { tit: 'TDD A', aut: 'Paulo Sousa', pag: 135, val: 169.99, categ: :tdd, isbn: '9983' },
-  { tit: 'PHP A', aut: 'Saulo Silva', pag: 239, val: 175.99, categ: :web, isbn: '9321' },
-  { tit: 'PHP C', aut: 'Jorge Alves', pag: 457, val: 230.15, categ: :web, isbn: '9148' },
-  { tit: 'TDD B', aut: 'Paulo Silva', pag: 351, val: 220.36, categ: :tdd, isbn: '5118' },
-  { tit: 'TDD C', aut: 'Saulo Alves', pag: 945, val: 199.32, categ: :tdd, isbn: '3148' },
-  { tit: 'SQL A', aut: 'Jorge Sousa', pag: 266, val: 190.99, categ: :dba, isbn: '8148' },
-  { tit: 'PHP B', aut: 'Paulo Alves', pag: 323, val: 175.99, categ: :web, isbn: '6148' },
-  { tit: 'SQL B', aut: 'Saulo Sousa', pag: 798, val: 320.99, categ: :dba, isbn: '4148' },
-  { tit: 'SQL C', aut: 'Jorge Silva', pag: 417, val: 199.99, categ: :dba, isbn: '1145' }
+  { tit: 'TDD A',
+    aut: 'Paulo Sousa',
+    pag: 135, val: 169.99,
+    categ: :tdd,
+    isbn: '9983' },
+  { tit: 'PHP A',
+    aut: 'Saulo Silva',
+    pag: 239,
+    val: 175.99,
+    categ: :web,
+    isbn: '9321' },
+  { tit: 'PHP C',
+    aut: 'Jorge Alves',
+    pag: 457,
+    val: 230.15,
+    categ: :web,
+    isbn: '9148' },
+  { tit: 'TDD B',
+    aut: 'Paulo Silva',
+    pag: 351,
+    val: 220.36,
+    categ: :tdd,
+    isbn: '5118' },
+  { tit: 'TDD C',
+    aut: 'Saulo Alves',
+    pag: 945,
+    val: 199.32,
+    categ: :tdd,
+    isbn: '3148' },
+  { tit: 'SQL A',
+    aut: 'Jorge Sousa',
+    pag: 266,
+    val: 190.99,
+    categ: :dba,
+    isbn: '8148' },
+  { tit: 'PHP B',
+    aut: 'Paulo Alves',
+    pag: 323,
+    val: 175.99,
+    categ: :web,
+    isbn: '6148' },
+  { tit: 'SQL B',
+    aut: 'Saulo Sousa',
+    pag: 798,
+    val: 320.99,
+    categ: :dba,
+    isbn: '4148' },
+  { tit: 'SQL C',
+    aut: 'Jorge Silva',
+    pag: 417,
+    val: 199.99,
+    categ: :dba,
+    isbn: '1145' }
 ]
 
 File.delete('midias.yml') if File.exist?('midias.yml')
+Dir.glob('db/revistas/*.yml').each { |file| File.delete(file) }
 # biblioteca = VendaFacil::Set.new
 biblioteca = Biblioteca.new
 livros.each do |livro|
@@ -23,18 +68,19 @@ livros.each do |livro|
                                 livro[:categ],
                                 livro[:isbn]))
 end
-puts '***** Executando o método livros_por_categoria com bloco (Biblioteca) *****'
+puts '*** Executando o método livros_por_categoria com bloco (Biblioteca) ***'
 biblioteca.midias_por_categoria :web do |midia|
   puts "Título: #{midia.titulo} - Categoria: #{midia.categoria}"
 end
-# Método não apresentará nenhuma resposta pois não foi passado o bloco como argumento
-puts '***** Executando o método livros_por_categoria sem bloco(Biblioteca) *****'
+# Matodo nao apresentara nenhuma resposta
+# pois nao foi passado o bloco como argumento
+puts '*** Executando o método livros_por_categoria sem bloco(Biblioteca) ***'
 puts biblioteca.midias_por_categoria(:dba)
 
 imprime_livro_no_console = lambda do |livro|
   puts "Título: #{livro.titulo} - Categoria: #{livro.categoria}"
 end
-puts '***** Executando o método livros_por_categoria com um lambda (Biblioteca) *****'
+puts '*** Executando o método livros_por_categoria lambda (Biblioteca) ***'
 biblioteca.midias_por_categoria(:tdd, &imprime_livro_no_console)
 
 puts '***** Executando o método total (Relatorio) *****'
@@ -50,10 +96,10 @@ dvd = DVD.new('Linux para desenvolvedores', 98.90, :linux)
 dvd.extend FormatadorMoeda
 puts "Livro. Título: #{livro.titulo} - Valor: #{livro.valor}"
 puts "DVD. Título: #{dvd.titulo} - Valor: #{dvd.valor}"
-puts "Livro. Título: #{livro.titulo} - Valor com desconto: #{livro.valor_com_desconto}"
-puts "Livro. Título: #{livro.titulo} - Valor formatado: #{livro.valor_formatado}"
-puts "DVD. Título: #{dvd.titulo} - Valor formatado: #{dvd.valor_formatado}"
-puts "DVD. Título: #{dvd.titulo} - Valor com desconto: #{dvd.valor_com_desconto}"
+puts "Livro. Título: #{livro.titulo} - Vl desconto: #{livro.valor_com_desconto}"
+puts "Livro. Título: #{livro.titulo} - Vl formatado: #{livro.valor_formatado}"
+puts "DVD. Título: #{dvd.titulo} - Vl formatado: #{dvd.valor_formatado}"
+puts "DVD. Título: #{dvd.titulo} - Vl desconto: #{dvd.valor_com_desconto}"
 
 puts 'Invocando o valor_com_desconto da Classe Midia'
 midia = Midia.new
@@ -65,7 +111,7 @@ p windows
 puts windows
 
 biblioteca_r = Biblioteca.new
-biblioteca.adiciona(Revista.new('MundoR'))
+biblioteca.adiciona(Revista.new('MundoR', 10.9))
 biblioteca.adiciona(DVD.new('Windows para desenvolvedores', 101.99, :windows))
 
 puts 'Biblioteca de revista'
@@ -78,3 +124,14 @@ windows = DVD.new 'Windows 7 for Dummies', 98.9, :sistemas_operacionais
 
 puts "DVD #{windows.valor_formatado}"
 puts "DVD com desconto #{windows.valor_com_desconto_formatado}"
+
+mundo_j = Revista.new 'Mundo J', 10.9
+puts "Valor original Revista: #{mundo_j.valor}"
+mundo_j.valor = 15.50
+mundo_j.save
+
+mundo_r = Revista.new 'Mundo R', 10.9
+mundo_r.save
+
+mundo_j2 = Revista.find 1
+p mundo_j2.valor
